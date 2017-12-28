1e8bbbba44dd31e1977963ce22fda180adb24ebf
Martin Mihálik <9matko.mihalik9@gmail.com>
2017-12-28 14:26:48 +0100

    Vytvorenie modelov pre druzinky a pokemonov

--- pokemoni/models.py
+++ pokemoni/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Druzinka(models.Model):
+    id = models.AutoField(primary_key=True)
+    nazov = models.CharField(max_length=100)
+
+class Pokemon(models.Model):
+    nazov = models.CharField(max_length=100)
+    druzinka = models.ForeignKey(Druzinka, on_delete=models.CASCADE)
+
+    sila = models.IntegerField(default=0)
+    rychlost = models.IntegerField(default=0)
+    postreh = models.IntegerField(default=0)
+    odolnost = models.IntegerField(default=0)
+
+    energia = models.IntegerField(default=10)
+    jedol = models.BooleanField(default=False)
