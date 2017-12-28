3c705d13f6d0a3a1697d43e756c5dff81d5396f7
MartinStevko <mstevko10@gmail.com>
2017-12-29 00:34:07 +0100

    Update models.py

--- pokemoni/models.py
+++ pokemoni/models.py
@@ -37,6 +37,8 @@ class Trener(models.Model):
 
     meno = models.CharField(max_length=100)
     cena = models.IntegerField(default=0)
+    vMeste = models.BooleanField(default=False)
+    
     qSila = models.IntegerField(default=0)
     qRychlost = models.IntegerField(default=0)
     qPostreh = models.IntegerField(default=0)
