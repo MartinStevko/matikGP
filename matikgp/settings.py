76e8bfd0d034e2c68e4e70816391cd5f8d41cc3a
MartinStevko <mstevko10@gmail.com>
2017-12-29 23:34:07 +0100

    Povolenie pristupu aj inac ako cez localhost

--- matikgp/settings.py
+++ matikgp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^f&sz6e5et=h#2_v48sqdc4ys(p8b#@x0hg8bt@jb)ac=z*wi!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
