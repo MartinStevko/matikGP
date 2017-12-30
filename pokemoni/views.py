375ba51556f8737deb1f1978cd9fc15164c60089
MartinStevko <mstevko10@gmail.com>
2017-12-30 22:56:59 +0100

    Timer

--- pokemoni/views.py
+++ pokemoni/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 
 from django.http import HttpResponseRedirect
-
-from django.shortcuts import reverse
 
 import time
 
