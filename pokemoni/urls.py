d5138669965c77aa7adadb5a5a0c46d8cc407b73
MartinStevko <mstevko10@gmail.com>
2017-12-30 05:28:26 +0100

    Pridanie url na druzinky

--- pokemoni/urls.py
+++ pokemoni/urls.py
@@ -7,5 +7,6 @@ app_name = 'pokemoni'
 urlpatterns = [
     path('trening', views.trening, name='trening'),
     path('obchod', views.obchod, name='obchod'),
-    path('prehlad', views.prehlad, name='prehlad')
+    path('prehlad', views.prehlad, name='prehlad'),
+    path('druzinka/<int:num>', views.druzinka, name='druzinka')
 ]
