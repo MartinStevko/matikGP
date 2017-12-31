84e1d63501b146433685deb8b88e3f385b89a003
MartinStevko <mstevko10@gmail.com>
2017-12-31 13:12:01 +0100

    Timer

--- pokemoni/admin.py
+++ pokemoni/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Druzinka, Pokemon, Trener, Kurz, Ucet
+from .models import Druzinka, Pokemon, Trener, Kurz, Ucet, Akcia
 
 @admin.register(Druzinka)
 class DruzinkaAdmin(admin.ModelAdmin):
@@ -10,3 +10,4 @@ admin.site.register(Pokemon)
 admin.site.register(Trener)
 admin.site.register(Kurz)
 admin.site.register(Ucet)
+admin.site.register(Akcia)
