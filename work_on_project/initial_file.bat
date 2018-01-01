217a4de422f00a58ff4089b735ed7597474d19b9
MartinStevko <mstevko10@gmail.com>
2018-01-01 05:31:19 +0200

    Inicializacny subor pre databazu #2

--- work_on_project/initial_file.bat
+++ work_on_project/initial_file.bat
@@ -1,17 +1,21 @@
+@echo off
+title Matik GP
+
 cd %~dp0
 cd ..
 pause
+
 manage.py makemigrations pokemoni
 manage.py migrate
 pause
+
 manage.py createsuperuser
 pause
+
 create_database.py
-pause
-
-REM !!! tu este treba daco dat !!!
-
+manage.py shell < init_database.txt
 del init_database.txt
-echo
-echo Successful!
+echo Successfully created!
 pause
+
+manage.py runserver 0.0.0.0:80
