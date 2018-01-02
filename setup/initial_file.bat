83d4de07e4fe0d2bbb5cf509af8ac46a782ce1e2
MartinStevko <mstevko10@gmail.com>
2018-01-02 12:13:57 +0200

    Folder rename

--- setup/initial_file.bat
+++ setup/initial_file.bat
@@ -7,15 +7,21 @@ pause
 
 manage.py makemigrations pokemoni
 manage.py migrate
+echo Migrations successfully created!
 pause
 
 manage.py createsuperuser
 pause
 
-create_database.py
-manage.py shell < init_database.txt
-del init_database.txt
-echo Successfully created!
+%~dp0\create_database.py
+manage.py shell < %~dp0\init_database.txt
+del %~dp0\init_database.txt
+echo Database successfully created!
+pause
+
+break>"%~dp0\zaciatok.txt"
+break>"%~dp0\koniec.txt"
+echo Required files successfully created!
 pause
 
 manage.py runserver 0.0.0.0:80
